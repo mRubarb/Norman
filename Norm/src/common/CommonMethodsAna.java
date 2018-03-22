d162ce923c89a80e2e4eee2d4ce6f20581e2798c
Ana Laura Pace <analaura.pace@gotocme.com>
2018-03-22 14:27:50 -0300

    New classes added

---
+++ Norm/src/common/CommonMethodsAna.java
@@ -0,0 +1,127 @@
+package common;
+
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import baseItems.BaseMain;
+
+public class CommonMethodsAna  extends BaseMain {
+
+	
+	public static List<WebElement> getSizesOfPages() {
+
+		return driver.findElements(By.xpath("//span/label/input[@name='pageSize']"));    
+		
+	}
+
+	
+	// Clicks on the page number at the bottom - pagination bar
+	public static void clickPageNumber(int page) throws InterruptedException {
+		
+		driver.findElement(By.xpath("//ul[@class='pagination']/li/a[contains(text()," + "\"" + page + "\"" + ")]")).click();
+		Thread.sleep(3000);
+	}
+	
+	
+	public static String GetNonRequiredItem(JSONObject jo,  String item) throws JSONException
+	{
+		try
+		{
+			jo.getString(item);				
+		}
+		catch (Exception e) 
+		{
+			return "";
+		}	
+		
+		return jo.getString(item);
+	}
+	
+	
+	// ENABLED/DISABLED values are converted to true/false
+	public static boolean convertToBoolean(String isEnabled) {
+		
+		if (isEnabled.equals("ENABLED")) {
+	
+			return true;
+			
+		} else if (isEnabled.equals("DISABLED")) {
+			
+			return false;
+			
+		}
+		return false; 
+	
+	}
+	
+	
+	public static void selectSizeOfList(int size) {
+		
+		System.out.println("Size to select: " + size);
+		
+		String xpath1 = "//span/label";
+		String xpath2 = "/input[@name='pageSize']";
+		
+		List<WebElement> sizeRadioButtons = driver.findElements(By.xpath(xpath1 + "/input"));
+		
+		for (int i = 0; i < sizeRadioButtons.size(); i++) {
+			
+			WebElement radioButtonSize = sizeRadioButtons.get(i); 
+						
+			if (radioButtonSize.getAttribute("value").equals(Integer.toString(size))) {
+				
+				driver.findElement(By.xpath(xpath1 + "[" + (i+1) + "]" + xpath2 + "/..")).click();
+				//driver.findElement(By.xpath(xpath + "[" + (i+1) + "]/..")).click();
+				break;
+			}
+								
+		}
+		
+	}
+	
+	
+	public static void clickArrowSorting(String sortBy, String sortDirection) throws InterruptedException {
+		
+		WebElement arrow = driver.findElement(By.xpath("//div[@class='table-responsive']/table/thead/tr/th/span[text()='" + sortBy + "']/following-sibling::span"));
+		
+		// If sortDirection is ASC and the arrow is not already ascending, click arrow once
+		if (sortDirection.equals("ASC") && !arrow.getAttribute("class").endsWith("sort-asc")) { 
+			
+			arrow.click();
+			
+		} 
+		// If sortDirection is DESC and the arrow is ascending, click arrow once
+		else if (sortDirection.equals("DESC") && arrow.getAttribute("class").endsWith("sort-asc")) { 
+			
+			arrow.click();
+			
+		}
+		// If sortDirection is DESC and the arrow is NOT ascending (2 arrows displayed), click arrow twice 
+		else if (sortDirection.equals("DESC") && !arrow.getAttribute("class").endsWith("sort-asc")) { 
+			
+			arrow.click();
+			arrow.click();
+			
+		} 
+		// If sortDirection is DESC and the arrow is already descending no click is needed  
+		else if (sortDirection.equals("DESC") && arrow.getAttribute("class").endsWith("sort-desc")) { 
+			// No clicks - already descending			
+		}
+		
+		Thread.sleep(2000);
+		
+	}
+
+	
+	public static String getTotalCountItems() {
+		
+		return driver.findElement(By.xpath("//div/jhi-item-count")).getText(); 
+		
+	}
+	
+	
+}
---
+++ Norm/src/testItems/TC_DeploymentsList.java
@@ -0,0 +1,58 @@
+package testItems;
+
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import org.json.JSONException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import baseItems.BaseMain;
+import common.CommonMethods;
+import pages.Deployments;
+
+
+public class TC_DeploymentsList extends BaseMain {
+
+	@BeforeClass
+	public static void setUp() throws Exception
+	{
+		SetupDiver();
+		OpenConsole();
+		LoginOneClick();
+		Thread.sleep(3000);
+	}
+	
+	
+	
+	@Test
+	public void testCase_DeploymentsList() throws InterruptedException, IOException, JSONException 
+	{
+					
+		CommonMethods.selectItemPlatformDropdown("Deployments");
+		CommonMethods.verifyTitle("Deployments");
+		
+		Deployments.verifyColumnsNames();
+		
+		Deployments.verifyData();
+		
+		Deployments.verifyDataAndSorting();
+		
+		Deployments.verifyPaging();
+		
+
+			
+	}
+	
+	
+	@AfterClass
+	public void finish() 
+	{
+		JOptionPane.showMessageDialog(null, "Ready To Close Driver.");		
+		driver.close();
+
+	}
+	
+}
